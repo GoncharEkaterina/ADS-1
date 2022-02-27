5d98c68c27a7cc9edbdc0b8cfe2bdcc1bcc7c365
GoncharenkoK <99185069+GoncharEkaterina@users.noreply.github.com>
2022-02-27 23:54:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <iostream>
+bool checkPrime(long long value);
+uint64_t nPrime(uint64_t n);
+uint64_t nextPrime(uint64_t value);
+uint64_t sumPrime(uint64_t hbound);
 
 int gcd(int a, int b) {
     if (b == 0)
       return a;
     return gcd(b, a % b);
-}
-
-
-bool checkPrime(uint64_t value) {
-    if (value == 2)
-      return true;
-    srand(time(NULL));
-    for (int i = 0; i < 100; i++) {
-        long long zh = (rand() % (value - 2)) + 2;
-        if (gcd(zh, value) != 1)
-          return false;
-        if (steps(zh, value - 1, value) != 1)
-          return false;
-    }
-  return true;
 }
 
 long long proverka(long long x, long long y, long long z) {
@@ -46,6 +36,19 @@ long long steps(long long x, long long y, long long z) {
   return (proverka(steps(x, y - 1, z), x, z) % z);
 }
 
+bool checkPrime(uint64_t value) {
+    if (value == 2)
+      return true;
+    srand(static_cast<unsingned int>(time(NULL)));
+    for (int i = 0; i < 100; i++) {
+        long long zh = (rand() % (value - 2)) + 2;
+        if (gcd(zh, value) != 1)
+          return false;
+        if (steps(zh, value - 1, value) != 1)
+          return false;
+    }
+  return true;
+}
 uint64_t nPrime(uint64_t n) {
     long long ch = 0;
     for (int num = 0; ; num++) {
@@ -73,4 +76,15 @@ uint64_t sumPrime(uint64_t hbound) {
         num++;
     }
   return mean;
+}
+
+int main () {
+    int a = 0;
+    std::cin >> a;
+    std::cout << checkPrime(a);
+    std::cout << nPrime(a);
+    std::cout << nextPrime(a);
+    std::cout << sumPrime(a);
+    
+    return 0;
 }
