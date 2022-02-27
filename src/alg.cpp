bbef36ecb2af0256d43e14a68c60eadca1548f43
GoncharenkoK <99185069+GoncharEkaterina@users.noreply.github.com>
2022-02-28 01:48:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
-#include <iostream>
 #include "alg.h"
 #include <stdlib.h>
 #include <math.h>
+#include <iostream>
 
 bool checkPrime(uint64_t value);
 uint64_t nPrime(uint64_t n);
